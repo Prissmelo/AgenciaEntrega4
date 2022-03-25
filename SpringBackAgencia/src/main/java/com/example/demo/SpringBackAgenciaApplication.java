def7cb24b24613017bb6c17742d4c04257c1a3b9
Priscila Melo <87330679+Prissmelo@users.noreply.github.com>
2022-03-24 22:25:24 -0300

    Add files via upload

---
+++ SpringBackAgencia/src/main/java/com/example/demo/SpringBackAgenciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackAgenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackAgenciaApplication.class, args);
+	}
+
+}
